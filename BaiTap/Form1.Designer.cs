db925242c8d72573a772ce67fd40329d2c91c2a9
Phong <nnphong130303@gmail.com>
2023-11-22 15:09:48 +0700

    TracNghiem

---
+++ BaiTap/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace BaiTap
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.ARadioButton = new System.Windows.Forms.RadioButton();
+            this.CRadioButton = new System.Windows.Forms.RadioButton();
+            this.BRadioButton = new System.Windows.Forms.RadioButton();
+            this.DRadioButton = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(61, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // ARadioButton
+            // 
+            this.ARadioButton.AutoSize = true;
+            this.ARadioButton.Location = new System.Drawing.Point(64, 132);
+            this.ARadioButton.Name = "ARadioButton";
+            this.ARadioButton.Size = new System.Drawing.Size(32, 17);
+            this.ARadioButton.TabIndex = 1;
+            this.ARadioButton.TabStop = true;
+            this.ARadioButton.Text = "A";
+            this.ARadioButton.UseVisualStyleBackColor = true;
+            // 
+            // CRadioButton
+            // 
+            this.CRadioButton.AutoSize = true;
+            this.CRadioButton.Location = new System.Drawing.Point(64, 205);
+            this.CRadioButton.Name = "CRadioButton";
+            this.CRadioButton.Size = new System.Drawing.Size(32, 17);
+            this.CRadioButton.TabIndex = 1;
+            this.CRadioButton.TabStop = true;
+            this.CRadioButton.Text = "C";
+            this.CRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // BRadioButton
+            // 
+            this.BRadioButton.AutoSize = true;
+            this.BRadioButton.Location = new System.Drawing.Point(226, 132);
+            this.BRadioButton.Name = "BRadioButton";
+            this.BRadioButton.Size = new System.Drawing.Size(32, 17);
+            this.BRadioButton.TabIndex = 1;
+            this.BRadioButton.TabStop = true;
+            this.BRadioButton.Text = "B";
+            this.BRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // DRadioButton
+            // 
+            this.DRadioButton.AutoSize = true;
+            this.DRadioButton.Location = new System.Drawing.Point(226, 205);
+            this.DRadioButton.Name = "DRadioButton";
+            this.DRadioButton.Size = new System.Drawing.Size(33, 17);
+            this.DRadioButton.TabIndex = 1;
+            this.DRadioButton.TabStop = true;
+            this.DRadioButton.Text = "D";
+            this.DRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(127, 273);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Next";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(419, 321);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.CRadioButton);
+            this.Controls.Add(this.DRadioButton);
+            this.Controls.Add(this.BRadioButton);
+            this.Controls.Add(this.ARadioButton);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton ARadioButton;
+        private System.Windows.Forms.RadioButton CRadioButton;
+        private System.Windows.Forms.RadioButton BRadioButton;
+        private System.Windows.Forms.RadioButton DRadioButton;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ BaiTap/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BaiTap
+{
+    public partial class Form1 : Form
+    {
+        private List<Question> questions;
+        private int currentQuestionIndex;
+        public Form1()
+        {
+            InitializeComponent();
+            questions = new List<Question>
+            {
+                new Question("what?", "A.A", "B.B", "C.C", "D.D", 'A'),
+                new Question("what?", "A.A", "B.B", "C.C", "D.D", 'B')
+            };
+            currentQuestionIndex = 0;
+            ShowQuestion(currentQuestionIndex);
+
+        }
+        private void ShowQuestion(int questionIndex)
+        {
+            if(questionIndex>=0 && questionIndex<questions.Count)
+            {
+                Question question = questions[questionIndex];
+                label1.Text = question.Text;
+                ARadioButton.Text = question.OptionA;
+                BRadioButton.Text = question.OptionB;
+                CRadioButton.Text = question.OptionC;
+                DRadioButton.Text = question.OptionD;
+                ARadioButton.Checked = false;
+                BRadioButton.Checked = false;
+                CRadioButton.Checked = false;
+                DRadioButton.Checked = false;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            char userAnswer = ' ';
+            if (ARadioButton.Checked)
+            {
+                userAnswer = 'A';
+            }
+            else if (BRadioButton.Checked)
+            {
+                userAnswer = 'B';
+            }
+            else if (CRadioButton.Checked)
+            {
+                userAnswer = 'C';
+            }
+            else if (DRadioButton.Checked)
+            {
+                userAnswer = 'D';
+            }
+
+            Question currentQuestion = questions[currentQuestionIndex];
+            if (userAnswer == currentQuestion.Answer)
+            {
+                MessageBox.Show("Dap An Dung!");
+            }
+            else
+            {
+                MessageBox.Show("Dap An Sai!");
+            }
+            currentQuestionIndex++;
+            if (currentQuestionIndex < questions.Count)
+            {
+                ShowQuestion(currentQuestionIndex);
+            }
+            else
+            {
+                MessageBox.Show("Hoan Thanh Cau Hoi");
+                Close();
+            }
+
+        }
+        public class Question
+        {
+            public string Text { get; }
+            public string OptionA { get; }
+            public string OptionB { get; }
+            public string OptionC { get; }
+            public string OptionD { get; }
+            public char Answer { get; }
+            public Question(string text, string optionA, string optionB, string optionC, string optionD, char answer)
+            {
+                //Text = text;
+                //OptionA = optionA;
+                //OptionB = optionB;
+                //OptionC = optionC;
+                //OptionD = optionD;
+                //Answer = answer;
+            }
+        }
+    }
+}
